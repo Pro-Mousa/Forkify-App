305f94b3f9027618633bff6a3b132a0611ba4bd6
Mousa Muigai <mousa@mousa.Mousa>
2025-10-08 18:49:30 +0300

    first commit

---
+++ src/js/controller.js
@@ -0,0 +1,33 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const recipeContainer = document.querySelector('.recipe');
+
+// NEW API URL (instead of the one shown in the video)
+// https://forkify-api.jonas.io
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
+    recipeView.renderSpinner();
+
+    // 1) Loading Recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering Recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
